fa6704009bba57a42a311518678fc42997e89586
edrovolt <ahmededrees30@gmail.com>
2024-05-27 22:10:38 +0300

    config: add "SpeedInsights" to collect performance metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "./ui/common/fonts";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Share Nights",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-mode="dark">
       <body className={`${inter.className} antialiased`}>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
