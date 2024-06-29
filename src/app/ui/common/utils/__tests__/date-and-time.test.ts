6a92f6b2ea61d0ba344a8d27661aa5894a6dbbbf
EdroVolt <ahmededrees30@gmail.com>
2024-06-29 12:56:22 +0300

    Fix: calculateTimeLeft test function

--- src/app/ui/common/utils/__tests__/date-and-time.test.ts
+++ src/app/ui/common/utils/__tests__/date-and-time.test.ts
@@ -3,7 +3,7 @@ import { calculateTimeLeft } from "../date-and-time";
 
 describe("calculateTimeLeft utility", () => {
   test("providing calculateTimeLeft with a date in string formate will return countdown object specifying the remaining time", () => {
-    const date = "2024-06-25T00:00:00";
+    const date = "5024-06-25T00:00:00";
     const timeLift = calculateTimeLeft(date);
 
     expect(timeLift.days).toBeDefined();
