d1c4a17d8fec66a835a5d3370a3bbfd0b3119c8a
EdroVolt <ahmededrees30@gmail.com>
2024-08-10 05:25:28 +0300

    fix: shift due date to December

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import CountdownTimer from "./ui/common/countdown-timer";
 
 export default function Home() {
-  const shareNightsReleaseDate = "2024-06-25T00:00:00";
+  const shareNightsReleaseDate = "2024-12-25T00:00:00";
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-background-100 text-primary-900 dark:bg-background-900 dark:text-primary-100">
